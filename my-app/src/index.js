d552a635a78b02f74e86b8902d96d67c23458bd4
Apostolis Apostolidis <github@toli.io>
2023-09-15 23:30:38 +0100

    add datadog rum

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { datadogRum } from '@datadog/browser-rum';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,7 +11,20 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+datadogRum.init({
+    applicationId: '272f4d04-f0d8-45cc-a2b5-5eb8d90452a4',
+    clientToken: 'pub49cb6f6888ae43bb1eaf65288cee263f',
+    site: 'datadoghq.eu',
+    service:'react-app-craco',
+    env:'local',
+    // Specify a version number to identify the deployed version of your application in Datadog 
+    // version: '1.0.0', 
+    sessionSampleRate:100,
+    sessionReplaySampleRate: 20,
+    trackUserInteractions: true,
+    trackResources: true,
+    trackLongTasks: true,
+    defaultPrivacyLevel:'mask-user-input'
+});
+    
+datadogRum.startSessionReplayRecording();
